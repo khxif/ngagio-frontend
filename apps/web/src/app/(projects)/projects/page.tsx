6ea46df9bc6d6403a7a01943360a229fd013f7be
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-11-06 15:47:52 +0530

    feat: completed projects page

---
+++ apps/web/src/app/(projects)/projects/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { Card, CardContent } from '@ngagio/components/ui/card';
+import { H3, Small } from '@ngagio/components/ui/typography';
+import { Navbar } from '@ngagio/layouts/projects-dashboard/navbar';
+import { useAuthStore } from '@ngagio/stores/auth-store';
+import { User } from '@ngagio/types';
+import { PlusIcon } from 'lucide-react';
+
+export default function ProjectsPage() {
+  const user = useAuthStore(state => state.user);
+
+  return (
+    <main>
+      <Navbar user={user as User} />
+
+      <section className="py-4 px-4 md:px-10 flex flex-col gap-8">
+        <H3 className="font-medium">Your Projects!</H3>
+
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <Card className="cursor-pointer">
+            <CardContent className="flex flex-col space-y-4 items-center justify-center h-full py-6">
+              <PlusIcon className="size-6" />
+              <Small className="text-lg font-medium">Create a New project.</Small>
+            </CardContent>
+          </Card>
+        </div>
+      </section>
+    </main>
+  );
+}
--- packages/layouts/src/projects-dashboard/navbar.tsx
+++ packages/layouts/src/projects-dashboard/navbar.tsx
@@ -2,10 +2,18 @@ import { UserButton } from '@ngagio/components/core/user-button';
 import { SidebarTrigger } from '@ngagio/components/ui/sidebar';
 import { User } from '@ngagio/types';
 
-export function Navbar({ user }: { user: User }) {
+interface NavbarProps {
+  user: User;
+  showSidebarTrigger?: boolean;
+}
+
+export function Navbar({ user, showSidebarTrigger = false }: NavbarProps) {
   return (
-    <nav className="flex items-center justify-between w-full py-2 border-b">
-      <SidebarTrigger />
+    <nav
+      className={`flex items-center w-full p-2 border-b 
+      ${showSidebarTrigger ? 'justify-between' : 'justify-end'}`}
+    >
+      {showSidebarTrigger ? <SidebarTrigger /> : null}
       <UserButton user={user} />
     </nav>
   );
